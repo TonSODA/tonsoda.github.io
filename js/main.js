3342d0525d2ccc3920d2de410599cd307f80fd59
Diego Raffa <diego170975@gmail.com>
2021-04-22 17:30:28 +0100

    added arrows in the showcase

--- js/main.js
+++ js/main.js
@@ -1,17 +1,30 @@
+// var swiper = new Swiper(".swiper-container", {
+//   effect: "coverflow",
+//   grabCursor: true,
+//   centeredSlides: true,
+//   slidesPerView: "auto",
+//   coverflowEffect: {
+//     rotate: 0,
+//     stretch: 0,
+//     depth: 200,
+//     modifier: 1,
+//     slideShadows: true,
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
+//   loop: true,
+// });
+
 var swiper = new Swiper(".swiper-container", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: "auto",
-  coverflowEffect: {
-    rotate: 0,
-    stretch: 0,
-    depth: 200,
-    modifier: 1,
-    slideShadows: true,
+  slidesPerView: 1,
+  spaceBetween: 30,
+  keyboard: {
+    enabled: true,
   },
-  pagination: {
-    el: ".swiper-pagination",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
   loop: true,
 });
